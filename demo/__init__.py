3ff7a297065cc51d7bbd0378c35819f4289b56aa
yaobin <yaobin@xiniaoyun.com>
2020-05-19 15:32:16 +0800

    同时提交两个

---
+++ demo/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/23 18:00
+# @Author  : Yao
+
---
+++ demo/demo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/23 16:53
+# @Author  : Yao
+
+def test():
+    print("aaaaaaaaaa")
